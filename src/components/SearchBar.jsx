3cda53046c2c5eaf4d9207f76b57429804bb2d6c
h3770k1k1 <amagdarass@gmail.com>
2023-07-26 08:36:01 +0200

    searchbar pre-done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
-
 const SearchBar = () => {
 	return (
 		<form>
 			<TextField
-				id="search-bar"
-				className="text"
-				variant="filled"
 				label="Lorem ipsum"
-				placeholder="Search..."
-			>
-				<IconButton type="submit" aria-label="search">
-					<SearchIcon />
-				</IconButton>
-			</TextField>
+				InputProps={{
+					endAdornment: (
+						<InputAdornment>
+							<IconButton>
+								<SearchIcon />
+							</IconButton>
+						</InputAdornment>
+					),
+				}}
+			/>
 		</form>
 	);
 };
