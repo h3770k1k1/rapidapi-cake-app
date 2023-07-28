0fc0dc4dc6f195b2ca15da949331fc4a2b0510f3
h3770k1k1 <amagdarass@gmail.com>
2023-07-28 20:49:40 +0200

    add recipes component to favouritespage

--- src/components/FavouritesPage.jsx
+++ src/components/FavouritesPage.jsx
@@ -3,13 +3,26 @@ import SearchBar from "./SearchBar";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import Grid from "@mui/material/Grid";
 import "./FavouritesPage.css";
+import RecipesList from "./RecipesList";
 
 const FavouritesPage = () => {
 	return (
 		<div className="Favourites">
-			<FavoriteIcon style={{ fontSize: 60 }} />
-			<h1>User's Favourites</h1>
-			<SearchBar />
+			<Grid
+				container
+				direction="column"
+				spacing={"1.5%"}
+				sx={{ marginTop: "1%", marginBottom: "1%" }}
+			>
+				<Grid item>
+					<FavoriteIcon style={{ fontSize: 60 }} />
+					<h1>XYZ's Favourites</h1>
+				</Grid>
+				<Grid item>
+					<SearchBar />
+				</Grid>
+			</Grid>
+			<RecipesList />
 		</div>
 	);
 };
