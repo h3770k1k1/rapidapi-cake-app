142abd24107fe5c1d54126916c86cec6f52b853e
h3770k1k1 <amagdarass@gmail.com>
2023-11-18 20:28:15 +0100

    sign in and out with google

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
 	Box,
 	Modal,
@@ -9,18 +9,34 @@ import {
 	Backdrop,
 	Fade,
 } from "@mui/material";
-import Logo from "./Logo";
-import SignUp from "./SignUp";
-import "./Login.css";
-import LoginPic from "./loginpic.png";
-import { auth } from "../firebase.js";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth, provider } from "../firebase";
+import { signInWithPopup, signOut } from "firebase/auth";
+import AuthDetails from "./AuthDetails"; // Import AuthDetails component
+import SignUp from "./SignUp";
+import LoginPic from "./loginpic.png";
 
 const LoginContainer = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
 	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	useEffect(() => {
+		// Check if the user is already logged in
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			if (user) {
+				setIsUserLoggedIn(true);
+			} else {
+				setIsUserLoggedIn(false);
+			}
+		});
+
+		return () => {
+			unsubscribe();
+		};
+	}, []);
+
 	const signIn = (e) => {
 		e.preventDefault();
 		signInWithEmailAndPassword(auth, email, password)
@@ -33,11 +49,18 @@ const LoginContainer = () => {
 			});
 	};
 
-	const signOut = () => {
-		// Dodaj kod do obsługi wylogowywania, jeśli to jest potrzebne
-		// Np. poprzez wywołanie funkcji signOut(auth)
-		setIsUserLoggedIn(false);
+	const handleClick = () => {
+		signInWithPopup(auth, provider).then((data) => {
+			setEmail(data.user.email);
+		});
 	};
+
+	const handleSignOut = () => {
+		signOut(auth).then(() => {
+			setIsUserLoggedIn(false);
+		});
+	};
+
 	const handleOpenModal = () => {
 		setIsModalOpen(true);
 	};
@@ -48,7 +71,7 @@ const LoginContainer = () => {
 
 	return (
 		<Container
-			maxWidth="xs" // Set the maxWidth to "xs" for a narrower container
+			maxWidth="xs"
 			sx={{
 				display: "flex",
 				flexDirection: "column",
@@ -58,8 +81,9 @@ const LoginContainer = () => {
 			}}
 		>
 			<div id="login-container">
+				<AuthDetails /> {/* Display user details and log out button */}
 				<img src={LoginPic} alt="Login" style={{ width: "60%" }} />
-				<form onSubmit={isUserLoggedIn ? signOut : signIn}>
+				<form onSubmit={isUserLoggedIn ? handleSignOut : signIn}>
 					<h1>{isUserLoggedIn ? "Sign Out" : "Log In to your Account"}</h1>
 					<TextField
 						type="email"
@@ -73,9 +97,16 @@ const LoginContainer = () => {
 						value={password}
 						onChange={(e) => setPassword(e.target.value)}
 					/>
-					<Button type="submit">
-						{isUserLoggedIn ? "Sign Out" : "Log In"}
-					</Button>
+					{isUserLoggedIn ? (
+						<Button type="button" onClick={handleSignOut}>
+							Sign Out
+						</Button>
+					) : (
+						<>
+							<Button type="submit">Log In</Button>
+							<button onClick={handleClick}>Sign In With Google</button>
+						</>
+					)}
 				</form>
 				<Typography
 					variant="h6"
@@ -95,7 +126,6 @@ const LoginContainer = () => {
 							color: "primary.main",
 						}}
 					>
-						{" "}
 						Sign up
 					</Typography>
 				</Typography>
--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,25 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-		apiKey: "AIzaSyChKh7NTfhrO3W8b-RGduOxdoIEpMXRg9w",
-		authDomain: "apicakesite.firebaseapp.com",
-		projectId: "apicakesite",
-		storageBucket: "apicakesite.appspot.com",
-		messagingSenderId: "716513426723",
-		appId: "1:716513426723:web:0d432bba8bb6476edca096",
-		measurementId: "G-QLGSWWPYL7"
+	apiKey: "AIzaSyChKh7NTfhrO3W8b-RGduOxdoIEpMXRg9w",
+	authDomain: "apicakesite.firebaseapp.com",
+	projectId: "apicakesite",
+	storageBucket: "apicakesite.appspot.com",
+	messagingSenderId: "716513426723",
+	appId: "1:716513426723:web:0d432bba8bb6476edca096",
+	measurementId: "G-QLGSWWPYL7",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app);
 const analytics = getAnalytics(app);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+export { auth, provider };
